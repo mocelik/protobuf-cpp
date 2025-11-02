6b6739bb54f708b0c3f92f3d44d0c0da5099713b
Muhammed Orhan Celik <morhancelik@gmail.com>
2025-11-01 23:28:45 -0400

    Updated serialization test cases

--- test/test_varint.cpp
+++ test/test_varint.cpp
@@ -1,16 +1,78 @@
 #include "protobuf-cpp/varint.h"
-#include "gtest/gtest.h"
-#include <limits>
 
-TEST(Varint, varint_encoded_size) {
+#include "gtest/gtest.h"
+
+#include <algorithm>
+#include <array>
+#include <limits>
+#include <ranges>
+
+// Precomputed maximum values for each possible varint size
+// equation: max_values[i] = (1 << (7 * (i + 1))) - 1
+constexpr std::array<std::uint64_t, 10> max_values = {
+    0x0000'0000'0000'007fULL, // 1 byte
+    0x0000'0000'0000'3fffULL, // 2 bytes
+    0x0000'0000'001f'ffffULL, // 3 bytes
+    0x0000'0000'0fff'ffffULL, // 4 bytes
+    0x0000'0007'ffff'ffffULL, // 5 bytes
+    0x0000'03ff'ffff'ffffULL, // 6 bytes
+    0x0001'ffff'ffff'ffffULL, // 7 bytes
+    0x00ff'ffff'ffff'ffffULL, // 8 bytes
+    0x7fff'ffff'ffff'ffffULL, // 9 bytes
+    0xffff'ffff'ffff'ffffULL  // 10 bytes
+};
+static_assert(max_values[9] == std::numeric_limits<std::uint64_t>::max(),
+              "Max value for 10-byte varint should be uint64_t max");
+
+/**
+ * Verify that encoding a varint produces the expected number of bytes.
+ */
+TEST(Varint, serialized_varint_encoded_size) {
 
     ASSERT_EQ(proto::serialize_varint(0).size(), 1);
-    for (int i = 1; i < 10; i++) {
-        uint64_t value = (1ULL << (7 * i)) - 1;
-        ASSERT_EQ(proto::serialize_varint(value).size(), i);
-        ASSERT_EQ(proto::serialize_varint(value + 1).size(), i + 1);
+
+    std::ranges::for_each(max_values, [](const auto &value) {
+        static int num_bytes = 1;
+        ASSERT_EQ(proto::serialize_varint(value).size(), num_bytes);
+        num_bytes++;
+    });
+
+    std::for_each(
+        max_values.begin(), max_values.end() - 1, [](const auto &value) {
+            static int num_bytes = 2;
+            ASSERT_EQ(proto::serialize_varint(value + 1).size(), num_bytes);
+            num_bytes++;
+        });
+}
+
+/**
+ * Verify that encoding a varint produces the expected byte values. There are
+ * too many values to check them all, so only the transition points are checked
+ */
+TEST(Varint, serialized_varint_encoded_value) {
+
+    // Check the 0-case
+    {
+        auto serialized = proto::serialize_varint(0);
+        ASSERT_EQ(serialized.size(), 1);
+        ASSERT_EQ(serialized[0], std::byte{0});
     }
-    ASSERT_EQ(proto::serialize_varint(std::numeric_limits<std::uint64_t>::max())
-                  .size(),
-              10);
+
+    // Check the transition points
+    std::ranges::for_each(max_values, [](const auto value) constexpr {
+        auto serialized = proto::serialize_varint(value);
+        std::for_each(
+            serialized.begin(), serialized.end() - 1,
+            [](const auto &byte) { ASSERT_EQ(byte, std::byte{0b1111'1111}); });
+
+        // Check if the last byte has the continue bit set, except for the max
+        // 10-byte varint
+        if (serialized.size() < 10) {
+            ASSERT_EQ(serialized.back(), std::byte{0b0111'1111});
+        } else {
+            // The 10th byte of the varint is oversized and won't be completely
+            // set
+            ASSERT_EQ(serialized.back(), std::byte{1});
+        }
+    });
 }
